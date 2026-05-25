cdb646bdfe0cbbbd5f07cd0f51a014e4254c6319
Danilson Sanches <dsanches06@outlook.com>
2026-05-25 18:12:08 +0100

    feat: cria stockRoutes e recipeItemRoutes

---
+++ backend/src/routes/stockRoutes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import * as stockController from "../controllers/stockController.js";
+
+const router = Router();
+
+router.get("/", stockController.getAll);
+router.get("/ingredient/:ingredientId", stockController.getByIngredientId);
+router.get("/:id", stockController.getById);
+router.post("/", stockController.create);
+router.put("/:id", stockController.update);
+router.patch("/ingredient/:ingredientId/adjust", stockController.adjustQuantity);
+router.delete("/:id", stockController.remove);
+
+export default router;
